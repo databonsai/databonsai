1abefe1a227b151702a0e0a66b4f133f8225307e
alvin-r <alvin.ryanputra@gmail.com>
2024-04-09 17:13:10 -0400

    changed readthedocs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,9 +19,7 @@ release = "0.2.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    "sphinx.ext.autodoc",
-]
+extensions = ["sphinx.ext.autodoc", "sphinx_rtd_theme"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
@@ -30,5 +28,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
